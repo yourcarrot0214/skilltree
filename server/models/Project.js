74afa11352e46513ad4d4f66dbcacfc4e3cdcc44
Carrot <stylenbs@gmail.com>
2021-04-09 15:35:38 +0900

    Project Model setup

---
+++ server/models/Project.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const projectSchema = mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  skills: {
+    type: Array,
+  },
+  members: {
+    type: Array,
+  },
+  leader: {
+    type: Object,
+  },
+  volunteer: {
+    type: Array,
+  },
+});
+
+const Project = mongoose.model("Project", projectSchema);
+
+module.exports = { Project };
+
+/*
+  Project Model
+    - Project 생성시 정보를 보관한다.
+
+  Schema
+    - title : project title. String
+    - description : 프로젝트 설명
+    - skills : 프로젝트에서 사용하는 스킬 목록. Array
+    - members : 프로젝트에 참가중인 유저 목록. Array
+    - leader : 프로젝트를 개설한 유저. Object
+    - volunteer : 프로젝트에 지원한 유저 목록. Array
+
+*/

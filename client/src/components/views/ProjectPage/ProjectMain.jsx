ce52f425a80f15ccdbd423d170eda9b62106e366
Carrot <stylenbs@gmail.com>
2021-07-02 18:03:01 +0900

    ProjectMain page SkillSearchBar 적용

--- client/src/components/views/ProjectPage/ProjectMain.jsx
+++ client/src/components/views/ProjectPage/ProjectMain.jsx
@@ -3,13 +3,19 @@ import { withRouter } from "react-router-dom";
 import { useSelector, shallowEqual } from "react-redux";
 
 import DetailPage from "../../common/DetailPage.jsx";
+import SkillSearchBar from "../../common/SkillSearchBar.jsx";
+import useSkills from "../../hooks/useSkills.js";
 
 const ProjectMain = () => {
+  const skills = useSkills();
   const projectState = useSelector((state) => state.project, shallowEqual);
+  const selectedSkills = skills.selectedSkills();
+  console.log(selectedSkills);
 
   return (
     <>
       <h3>Project Main Page</h3>
+      <SkillSearchBar selected />
       <DetailPage location='Project' classList={projectState} />
     </>
   );

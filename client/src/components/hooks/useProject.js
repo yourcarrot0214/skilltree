2a560eadcdaff4ba9d085ec5dbdd394c33502f04
Carrot <stylenbs@gmail.com>
2021-07-06 18:27:19 +0900

    profile page에서 유저 프로젝트 관리 기능 구현

--- client/src/components/hooks/useProject.js
+++ client/src/components/hooks/useProject.js
@@ -1,7 +1,19 @@
 import { useSelector, shallowEqual } from "react-redux";
 
-const useProject = (userId) => {
-  const projectList = useSelector((state) => state.project, shallowEqual);
+const useProject = () => {
+  const projectState = useSelector((state) => state.project, shallowEqual);
+
+  projectState.leader = function (userId) {
+    return projectState.filter((project) => project.leader === userId);
+  };
+
+  projectState.findProject = function (projectIdList) {
+    return projectIdList.map((projectId) =>
+      projectState.find((project) => project._id === projectId)
+    );
+  };
+
+  return projectState;
 };
 
 export default useProject;
--- client/src/components/views/ProfilePage/accont/Project.jsx
+++ client/src/components/views/ProfilePage/accont/Project.jsx
@@ -1,17 +1,70 @@
-import React, { useState } from "react";
+import React from "react";
+import useProject from "../../../hooks/useProject.js";
+import { ClassCardContainer } from "../../../common/styles/styled";
+import ClassCard from "../../../common/class/ClassCard.jsx";
 
 const Project = ({ userData }) => {
-  const [projectLeader, setProjectLeader] = useState(userData.project.leader);
-  const [projectMember, setProjectMember] = useState(userData.project.member);
-  const [projectApply, setProjectApply] = useState(userData.project.apply);
-  console.log(`projectLeader : ${projectLeader}`);
-  console.log(`projectMember : ${projectMember}`);
-  console.log(`projectApply : ${projectApply}`);
+  const projectList = useProject();
+  const projectLeader = projectList.leader(userData._id);
+  const projectMember = projectList.findProject(userData.project.member);
+  const projectApply = projectList.findProject(userData.project.apply);
+
   return (
     <>
       <div>운영중인 프로젝트</div>
+      <ClassCardContainer>
+        {projectLeader.map((project) => (
+          <ClassCard
+            key={project._id}
+            id={project._id}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            leader={project.leader}
+            personnel={project.personnel}
+            members={project.members}
+            status={project.status}
+            location='Project'
+            volunteer={project.volunteer}
+          />
+        ))}
+      </ClassCardContainer>
       <div>참가중인 프로젝트</div>
+      <ClassCardContainer>
+        {projectMember.map((project) => (
+          <ClassCard
+            key={project._id}
+            id={project._id}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            leader={project.leader}
+            personnel={project.personnel}
+            members={project.members}
+            status={project.status}
+            location='Project'
+            volunteer={project.volunteer}
+          />
+        ))}
+      </ClassCardContainer>
       <div>지원중인 프로젝트</div>
+      <ClassCardContainer>
+        {projectApply.map((project) => (
+          <ClassCard
+            key={project._id}
+            id={project._id}
+            title={project.title}
+            description={project.description}
+            skills={project.skills}
+            leader={project.leader}
+            personnel={project.personnel}
+            members={project.members}
+            status={project.status}
+            location='Project'
+            volunteer={project.volunteer}
+          />
+        ))}
+      </ClassCardContainer>
     </>
   );
 };

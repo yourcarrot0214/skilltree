732e17e0facdff32c0fae8ef933f6b8732c9c9a4
Carrot <stylenbs@gmail.com>
2021-07-13 20:30:37 +0900

    Header Component props 구조변경

--- client/src/components/views/LandingPage/Header.jsx
+++ client/src/components/views/LandingPage/Header.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { HeaderContainer } from "./styles/styled.js";
-const HeaderTitle = "가르치고 배우며 함께 성장합니다.";
-const SubTitle =
-  "배우고 싶은 스킬로 스터디에 참가하고, 다룰 수 있는 스킬로 프로젝트에 참가하세요!";
 
-const Header = () => {
+const Header = ({ title, subTitle }) => {
   return (
     <HeaderContainer>
-      <h1>{HeaderTitle}</h1>
-      <p>{SubTitle}</p>
+      <h1>{title}</h1>
+      <p>{subTitle}</p>
     </HeaderContainer>
   );
 };
--- client/src/components/views/LandingPage/LandingPage.jsx
+++ client/src/components/views/LandingPage/LandingPage.jsx
@@ -1,35 +1,17 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { LandingPageContainer } from "./styles/styled.js";
-// import { useDispatch } from "react-redux";
-// import { chooseOneSelected } from "../../../_actions/skill_action.js";
+
 import Test from "../../../_reducers/Test.jsx";
 import Header from "./Header.jsx";
-import SkillSearchBar from "../../common/SkillSearchBar.jsx";
-
-// import useSkills from "../../hooks/useSkills.js";
-// import useProject from "../../hooks/useProject.js";
-// import useStudy from "../../hooks/useStudy.js";
 
 function LandingPage() {
-  // const dispatch = useDispatch();
-  // const skillsState = useSkills();
-  // const projectState = useProject();
-  // const studyState = useStudy();
-  // const selectedSkills = skillsState.selectedSkills();
-  // const skillIdList = selectedSkills.map((skill) => skill._id);
-
-  // const relatedUsers = skillsState.relatedUsers(selectedSkills);
-  // const relatedProject = projectState.relatedProject(skillIdList);
-
-  // const onClickFunction = (e) => {
-  //   dispatch(chooseOneSelected(e.target.id));
-  // };
-
   return (
     <LandingPageContainer>
-      <Header />
-      {/* <SkillSearchBar selected={true} onClickFunction={onClickFunction} /> */}
+      <Header
+        title='가르치고 배우며 함께 성장합니다.'
+        subTitle='배우고 싶은 스킬로 스터디에 참가하고, 다룰 수 있는 스킬로 프로젝트에 참가하세요!'
+      />
       <Test />
     </LandingPageContainer>
   );

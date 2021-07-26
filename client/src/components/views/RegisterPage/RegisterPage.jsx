bfe7cd8cd94c756f979ecc2883519b607b118fe6
Carrot <stylenbs@gmail.com>
2021-07-26 18:46:22 +0900

    register page css update

--- client/src/components/views/RegisterPage/RegisterPage.jsx
+++ client/src/components/views/RegisterPage/RegisterPage.jsx
@@ -1,21 +1,13 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import {
+  SubmitContainer,
+  SubmitWrapper,
+  SubmitImage,
+  SubmitForm,
+} from "./styles/styled.js";
 import { registerUser } from "../../../_actions/user_action.js";
 import { withRouter } from "react-router-dom";
 import { useDispatch } from "react-redux";
-
-const SubmitContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100vh;
-`;
-
-const SubmitForm = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
 
 function RegisterPage(props) {
   const dispatch = useDispatch();
@@ -40,6 +32,11 @@ function RegisterPage(props) {
         props.history.push("/login");
       } else {
         console.log("Sign Up Filed :: ", response.payload.err);
+        if (response.payload.err.keyPattern.email === 1) {
+          alert("이미 가입된 이메일 주소 입니다.");
+        } else {
+          alert(response.payload.err.message);
+        }
       }
     });
   };
@@ -55,41 +52,44 @@ function RegisterPage(props) {
 
   return (
     <SubmitContainer>
-      <SubmitForm onSubmit={onSignUpSubmit}>
-        <label>Name</label>
-        <input
-          type='text'
-          name='name'
-          value={Name}
-          onChange={onChangeValue}
-          required
-        />
-        <label>Email</label>
-        <input
-          type='email'
-          name='email'
-          value={Email}
-          onChange={onChangeValue}
-          required
-        />
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          value={Password}
-          onChange={onChangeValue}
-          required
-        />
-        <label>Password</label>
-        <input
-          type='password'
-          name='confirm-password'
-          value={ConfirmPassword}
-          onChange={onChangeValue}
-          required
-        />
-        <button type='submit'>SIGN UP</button>
-      </SubmitForm>
+      <SubmitWrapper>
+        <SubmitImage />
+        <SubmitForm onSubmit={onSignUpSubmit}>
+          <label>Name</label>
+          <input
+            type='text'
+            name='name'
+            value={Name}
+            onChange={onChangeValue}
+            required
+          />
+          <label>Email</label>
+          <input
+            type='email'
+            name='email'
+            value={Email}
+            onChange={onChangeValue}
+            required
+          />
+          <label>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={Password}
+            onChange={onChangeValue}
+            required
+          />
+          <label>Password Check</label>
+          <input
+            type='password'
+            name='confirm-password'
+            value={ConfirmPassword}
+            onChange={onChangeValue}
+            required
+          />
+          <button type='submit'>SIGN UP</button>
+        </SubmitForm>
+      </SubmitWrapper>
     </SubmitContainer>
   );
 }
---
+++ client/src/components/views/RegisterPage/styles/styled.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import backgroundImage from "../images/register-image1.png";
+
+const SubmitContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 90%;
+`;
+
+const SubmitWrapper = styled.div`
+  width: 100%;
+  background-color: #dee2e6;
+  display: flex;
+  flex-direction: row;
+  max-width: 1024px;
+  padding: 1rem;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 1rem;
+  box-shadow: 20px 20px 60px #bdc0c4, -20px -20px 60px #ffffff;
+`;
+
+const SubmitImage = styled.div`
+  width: 400px;
+  height: 400px;
+  background-image: url(${backgroundImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const SubmitForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  justify-content: center;
+  align-items: center;
+  width: 400px;
+  height: 400px;
+
+  label {
+    width: 300px;
+    font-size: 1rem;
+    color: #495057;
+  }
+
+  input {
+    width: 300px;
+    height: 30px;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    border-bottom: 3px solid #343a40;
+    background-color: rgba(255, 255, 255, 0);
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: #343a40;
+    margin-bottom: 1rem;
+
+    ::placeholder {
+      color: #495057;
+    }
+
+    :focus {
+      outline: none;
+      border-bottom: 3px solid #40c057;
+    }
+  }
+
+  button {
+    padding: 0.7rem;
+    margin: 1rem auto;
+    border: none;
+    border-radius: 0.3rem;
+    background-color: #51cf66;
+    color: white;
+    width: 300px;
+    font-size: 1rem;
+    font-weight: bold;
+
+    :hover {
+      background-color: #40c057;
+      transition: 0.5s;
+    }
+  }
+`;
+
+export { SubmitContainer, SubmitWrapper, SubmitImage, SubmitForm };

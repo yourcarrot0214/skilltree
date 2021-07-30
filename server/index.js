61ffcb8169f594b842d8ce8ee36269524e062e60
Carrot <stylenbs@gmail.com>
2021-07-30 21:39:24 +0900

    heroku setup

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const PORT = 5000;
 const CONFIG = require("./config/key.js");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
 
 const usersRouter = require("./routes/usersRouter.js");
@@ -31,5 +34,12 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello express"));
+
+if (process.env.NODE_ENV === "projuction") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Express app listening on Port ${PORT}`));

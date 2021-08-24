45b47fcdcd668196a0340ea31606d1cb4bcca4ee
yourcarrot0214 <stylenbs@gmail.com>
2021-08-24 16:59:39 +0900

    오타 수정

--- server/config/key.js
+++ server/config/key.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === "prodution") {
+if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod.js");
 } else {
   module.exports = require("./dev.js");

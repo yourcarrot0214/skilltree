bf082d17b2bd2e2d7df00bcc5ea30ad7bd4c9851
yourcarrot0214 <stylenbs@gmail.com>
2021-08-27 15:04:06 +0900

    path setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 5000;

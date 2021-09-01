1ea88317299d4aa71da8eeb72e4d1d9aa32bc887
yourcarrot0214 <stylenbs@gmail.com>
2021-09-01 20:09:37 +0900

    LandingPage Header Link 추가

--- client/src/components/views/LandingPage/Header.jsx
+++ client/src/components/views/LandingPage/Header.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { HeaderContainer, LinkBox } from "./styles/styled.js";
+import useUserData from "../../hooks/useUserData";
 
 const Header = ({ title, subTitle }) => {
+  const { isAuth } = useUserData();
   return (
     <HeaderContainer>
       <h1>{title}</h1>
       <p>{subTitle}</p>
-      <LinkBox signup>
-        <Link to='/register'>Sign Up</Link>
-      </LinkBox>
+      {isAuth ? (
+        <LinkBox signup>
+          <Link to='/profile'>Profile 바로가기</Link>
+        </LinkBox>
+      ) : (
+        <LinkBox signup>
+          <Link to='/register'>Sign Up</Link>
+        </LinkBox>
+      )}
     </HeaderContainer>
   );
 };
